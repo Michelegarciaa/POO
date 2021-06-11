526775eb8e12fbbfa72902e4940c958c602c8d04
SERGIO ANTONIO A BARBOSA <saabarbosa@gmail.com>
2021-06-11 17:06:42 -0300

    commite

---
+++ Aula01_Classe_Objeto/Principal.java
@@ -0,0 +1,10 @@
+public class Principal {
+  static public void main(String[] parametros)
+	{
+    Produto prod = new Produto();
+    prod.setCodigo(1);
+    prod.setNome("Sabao craca");
+    prod.setPreco(1.50);
+    System.out.println(prod.getCodigo() + "\t" + prod.getNome() + "\t" + prod.getPreco());
+  }
+}
